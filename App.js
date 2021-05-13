a02cfacabff247ffe268069bc8b7fae6ce18879a
Don Hudson <don@hostmerchantservices.com>
2021-05-12 21:04:23 -0400

    header added to shopping list app]

--- App.js
+++ App.js
@@ -1,12 +1,21 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
+import Header from './components/Header'
 
 const App = () => {
   return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>Testing react native app</Text>
+    <View style={styles.container }>
+      <Header title="Shopping List"/>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 30 
+  },
+
+})
 
 export default App
---
+++ components/Header.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const Header = ({title}) => {
+    return (
+        <View style={styles.header}>
+            <Text style={styles.text}>{title}</Text>
+        </View>
+    )
+}
+
+export default Header
+
+const styles = StyleSheet.create({
+    header: {
+        height: 60,
+        padding: 15,
+        backgroundColor: '#ee6e73'
+    },
+
+    text: {
+        color: '#fff',
+        fontSize: 23,
+        textAlign: 'center',
+    }    
+})
